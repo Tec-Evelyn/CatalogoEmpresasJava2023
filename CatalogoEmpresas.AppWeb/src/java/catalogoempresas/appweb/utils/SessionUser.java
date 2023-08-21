e9012ddf5a641eb3560448b609ab49009018c852
Evelyn <evelynliquez04@outlook.com>
2023-08-21 08:15:58 -0600

    Codificacion de los elementos del paquete utils del proyecto AppWeb

--- CatalogoEmpresas.AppWeb/src/java/catalogoempresas/appweb/utils/SessionUser.java
+++ CatalogoEmpresas.AppWeb/src/java/catalogoempresas/appweb/utils/SessionUser.java
@@ -1,5 +1,55 @@
 package catalogoempresas.appweb.utils;
 
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import catalogoempresas.entidadesdenegocio.*;
+
 public class SessionUser {
+    public static void autenticarUser(HttpServletRequest request, Usuario pUsuario) {
+        HttpSession session = (HttpSession) request.getSession();//si ecribo algo entre parentesis y luego algo mas fuera de los parentesis se esta haciendo una conversion
+        session.setMaxInactiveInterval(3600);//Cantidad maxima que estara abrierta una sesion
+        session.setAttribute("auth", true);
+        session.setAttribute("user", pUsuario.getLogin());
+        session.setAttribute("rol", pUsuario.getRol().getNombre()); 
+    }
     
+    public static boolean isAuth(HttpServletRequest request) {
+        HttpSession session = (HttpSession) request.getSession();
+        boolean auth = session.getAttribute("auth") != null ? (boolean) session.getAttribute("auth") : false;//si el parametro "auth" es diferente de nullo lo tamamos y lo convertimos en booleano y eso es lo que devolvemos de lo cantrario devolvemos una cadena vacia
+        return auth;
+    }
+    
+    public static String getUser(HttpServletRequest request) {
+        HttpSession session = (HttpSession) request.getSession();
+        String user = "";
+        if (SessionUser.isAuth(request)) { 
+            user = session.getAttribute("user") != null ? (String) session.getAttribute("user") : "";
+        }
+        return user;
+    }
+    
+    public static String getRol(HttpServletRequest request) {
+        HttpSession session = (HttpSession) request.getSession();
+        String user = "";
+        if (SessionUser.isAuth(request)) {
+            user = session.getAttribute("rol") != null ? (String) session.getAttribute("rol") : "";//Operador ternario: es como una candiocion IF pero en una sola linea
+        }
+        return user;
+    }
+    
+    public static void authorize(HttpServletRequest request, HttpServletResponse response, IAuthorize pIAuthorize) throws ServletException, IOException {
+        if (SessionUser.isAuth(request)) {//si el usuario ya inicio sesion
+            pIAuthorize.authorize();//JDK
+        } else {
+            response.sendRedirect("Usuario?accion=login");//si no ha iniciado sesion lo mandamos a que inicie
+        }
+    }
+    
+    public static void cerrarSession(HttpServletRequest request) {
+        HttpSession session = (HttpSession) request.getSession();//del metodo request obtenemos la sesion
+        session.invalidate();
+    }
 }
--- CatalogoEmpresas.AppWeb/src/java/catalogoempresas/appweb/utils/Utilidad.java
+++ CatalogoEmpresas.AppWeb/src/java/catalogoempresas/appweb/utils/Utilidad.java
@@ -1,5 +1,28 @@
 package catalogoempresas.appweb.utils;
 
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class Utilidad {
+    public static String getParameter(HttpServletRequest request, String pKey, String pDefault) {
+        String result = "";//declaramos la variable result y la inicializamos entre comillas
+        String value = request.getParameter(pKey);//metodo que no0s mostrara en que peticion estamos
+        if (value != null && value.trim().length() > 0) {//condicion: valodamos que el valor no sea nullo y que no este vacia la cadena
+            result = value;
+        } else {
+            result = pDefault;
+        }
+        return result;
+    }
     
+    public static void enviarError(String pError, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("error", pError); // peticion que le asignamos lo esta entrando en la linea anterior
+        request.getRequestDispatcher("Views/Shared/error.jsp").forward(request, response);//que navege a la jsp
+    }
+    
+    public static String obtenerRuta(HttpServletRequest request, String pStrRuta) {
+        return request.getContextPath() + pStrRuta;//getContextPath() me dice cual es la url  de mi servidor
+    }
 }
